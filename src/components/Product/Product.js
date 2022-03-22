291417556741aeeaa301114ee7f027786e5b65b3
Your Name <sarifulislaml8280@gmail.com>
2022-03-22 15:10:01 +0600

    card added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    console.log(props.product);
+    const { name, img, seller, price, ratings } = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt=""></img>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings}</small></p>
+            </div>
+            <button className='btn-card'>
+                <p>Add to card</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
