041f7ccfa1f8f36dd9fb108dd21ca0af3c0d227a
Your Name <sarifulislaml8280@gmail.com>
2022-03-23 16:55:15 +0600

    trying to solve local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,8 +4,10 @@ const Cart = ({ cart }) => {
 
     let total = 0
     let shipping = 0
+    let quantity = 0
     for (const product of cart) {
-        total = total + product.price
+        quantity = quantity + product.quantity
+        total = total + product.price * product.quantity
         shipping = shipping + product.shipping
     }
     const tax = (total * 0.1).toFixed(2)
@@ -13,7 +15,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
